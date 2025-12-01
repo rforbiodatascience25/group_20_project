128d8cf05bdb5d4fbbfcd502a545453d3f4d695b
QJ-ChenR <s252613@dtu.dk>
2025-12-01 09:20:45 +0100

    coding style

--- R/99_proj_func.R
+++ R/99_proj_func.R
@@ -30,3 +30,40 @@ save_and_show_table <- function(input_table, table_name, save_location = "../res
   readr::write_csv(input_table, str_c(save_location, table_name, filetype))
   print(input_table)
 }
+
+# function to calculate KM
+km_fit <- function(data, time_col, event_col, group_col) {
+  split(data, data[[group_col]]) %>% 
+    map_dfr(function(df) {
+      
+      # time order
+      df <- df[order(df[[time_col]]), ]
+      
+      # make table
+      tab <- table(df[[time_col]], df[[event_col]])
+      
+      # get data
+      times <- as.numeric(rownames(tab))
+      
+      # get event
+      n_event <- if ("1" %in% colnames(tab)) tab[, "1"] else 0
+      
+      # get sum
+      n_drop <- rowSums(tab)
+      
+      # cal risk
+      n_risk <- rev(cumsum(rev(n_drop)))
+      
+      # cal surv_prob
+      surv_prob <- cumprod(1 - n_event / n_risk)
+      
+      # return results
+      tibble(
+        group   = df[[group_col]][1],
+        time    = times,
+        n_risk  = n_risk,
+        n_event = n_event,
+        surv    = surv_prob
+      )
+    })
+}
